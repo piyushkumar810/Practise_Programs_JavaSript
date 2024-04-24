f0d4b76b0591ef840397da880f4bb5b0b3577f81
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-04-24 19:39:42 +0530

    function

---
+++ q7.js
@@ -0,0 +1,6 @@
+function myfun()
+{
+    console.log("hey");
+}
+// myfun ko yaha per object ki tarha treat kar rha hai js mai or js mai hum name key ka use kar ke kisi v function ka name GET kar sakti hai
+console.log(myfun.name);
