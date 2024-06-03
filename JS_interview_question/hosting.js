cc7b72df51d91a6ab0f20e20956b55b8eb2d47c1
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-06-03 23:44:16 +0530

    concept of hosting

---
+++ JS_interview_question/hosting.js
@@ -0,0 +1,13 @@
+
+var a=10;
+
+function myfun()
+{
+    console.log("this is my function");
+}
+// console.log(a);
+
+// abhi ye code execute nhi hua hai fir v isse memory mai space mil chuka hai
+
+myfun();
+console.log(a);
